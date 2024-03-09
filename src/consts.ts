919daba1bdfe5c221776d570a9b37a6f4fa5445b
Naoki Haba <naosprintrunner6385@gmail.com>
2024-03-09 15:21:31 +0900

    Add initial project configurations

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+export const SITE_METADATA = {
+    title: 'naoki.dev',
+}
+
+export const NAV_ITEMS = [
+    { label: 'Home', path: '/' },
+    { label: 'Blog', path: '/blog' },
+    { label: 'About', path: '/about' },
+]
